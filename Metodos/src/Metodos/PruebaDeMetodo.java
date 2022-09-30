62139bc99893cf7b666917d69a633f3c64102b23
JosueB10 <josuebeteta8@gmail.com>
2022-09-30 17:45:35 -0600

    Primera clase de metodos

---
+++ Metodos/src/Metodos/PruebaDeMetodo.java
@@ -0,0 +1,13 @@
+package Metodos;
+
+public class PruebaDeMetodo {
+
+	public static void main(String[] args) {
+		new MetodoOFuncion2Version3().calcularRaicesCuadraticas();
+		MetodoOFuncion2Version3.calcularRaicesCuadraticas();
+		//calcularRaicesCuadraticas();
+		
+		MetodoOFuncion2Version3.calcularRaicesCuadraticas();
+	}
+
+}
